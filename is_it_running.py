0a385716d1b33c8e2066e8eb7a84eb8508c89d5f
Dextority <48394213+Zane285@users.noreply.github.com>
2022-04-10 21:36:00 -0500

    Update is_it_running.py
    Added Valorant support

--- is_it_running.py
+++ is_it_running.py
@@ -1,5 +1,5 @@
 import psutil, os
-print("Looking For Processes")
+vpnName = input("Enter your OpenVPN config name: ")
 while True:
     if "openvpn.exe" in (p.name() for p in psutil.process_iter()):
         if "FortniteClient-Win64-Shipping.exe" in (p.name() for p in psutil.process_iter()):
@@ -8,12 +8,29 @@ while True:
                 os.system("taskkill.exe /F /IM openvpn.exe")
             except Exception as e:
                 print(e)
-
-    elif "FortniteClient-Win64-Shipping.exe" not in (p.name() for p in psutil.process_iter()):
-        if "openvpn.exe" not in (p.name() for p in psutil.process_iter()):
+    elif "openvpn.exe" not in (p.name() for p in psutil.process_iter()):
+        if "FortniteClient-Win64-Shipping.exe" not in (p.name() for p in psutil.process_iter()):
+            if "VALORANT.exe" not in (p.name() for p in psutil.process_iter()):
+                try:
+                    print("initializing vpn")
+                    cmd = 'start /b cmd /c "C:\\Program Files\\OpenVPN\\bin\\openvpn-gui.exe" --connect vpnName.ovpn'
+                    os.system(cmd)
+                except Exception as e:
+                    print(e)
+    if "openvpn.exe" in (p.name() for p in psutil.process_iter()):
+        if "VALORANT.exe" in (p.name() for p in psutil.process_iter()):
+            print(f"Detected Valorant and OpenVPN\nKilling OpenVPN")
             try:
-                print("initializing vpn")
-                cmd = 'start /b cmd /c "C:\\Program Files\\OpenVPN\\bin\\openvpn-gui.exe" --connect VpnNameHere.ovpn'
-                os.system(cmd)
+                os.system("taskkill.exe /F /IM openvpn.exe")
             except Exception as e:
                 print(e)
+
+    elif "openvpn.exe" not in (p.name() for p in psutil.process_iter()):
+        if "FortniteClient-Win64-Shipping.exe" not in (p.name() for p in psutil.process_iter()):
+            if "VALORANT.exe" not in (p.name() for p in psutil.process_iter()):
+                try:
+                    print("initializing vpn")
+                    cmd = 'start /b cmd /c "C:\\Program Files\\OpenVPN\\bin\\openvpn-gui.exe" --connect vpnName.ovpn'
+                    os.system(cmd)
+                except Exception as e:
+                    print(e)
